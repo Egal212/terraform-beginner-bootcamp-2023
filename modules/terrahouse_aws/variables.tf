bd28cd3db0ae7634ce92a581255e0d45011d2433
Glory Ehis <114033502+Egal212@users.noreply.github.com>
2023-10-24 15:04:20 +0100

    Update variables.tf

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -8,36 +8,36 @@ variable "user_uuid" {
       }
     }
 
-    #variable "index_html_filepath" {
-      #description = "The file path for index.html"
-      #type = string
+    variable "index_html_filepath" {
+      description = "The file path for index.html"
+      type = string
 
-      #validation {
-        #condition = fileexists(var.index_html_filepath)
-        #error_message = "The provided path for index.html does not exist"
-      #}
-   # }
+      validation {
+        condition = fileexists(var.index_html_filepath)
+        error_message = "The provided path for index.html does not exist"
+      }
+   }
     
-     #variable "error_html_filepath" {
-      #description = "The file path for error.html"
-      #type = string
+     variable "error_html_filepath" {
+      description = "The file path for error.html"
+      type = string
 
-      #validation {
-        #condition = fileexists(var.error_html_filepath)
-        #error_message = "The provided path for error.html does not exist"
-     # }
-   # }
-# variable "bucket_name" {
-#   description = "The name of the S3 bucket"
-#   type = string
+      validation {
+        condition = fileexists(var.error_html_filepath)
+        error_message = "The provided path for error.html does not exist"
+    }
+    }
+ variable "bucket_name" {
+   description = "The name of the S3 bucket"
+   type = string
 
 
 
-#   validation {
-#     condition        = (
-#       length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
-#       can(regex("^[a-z0-9[a-z0-9-.]*[a-z0-9]$",var.bucket_name))
-#     )
-#     error_message = "The bucket name must be between 3 and 63 characters"
-#   }
-# }
+   validation {
+     condition        = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
+       can(regex("^[a-z0-9[a-z0-9-.]*[a-z0-9]$",var.bucket_name))
+     )
+   error_message = "The bucket name must be between 3 and 63 characters"
+   }
+ }
